5996b400f5d335a365041c07271954ff4cdf7add
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-08 19:15:53 +0800

    Create user model and table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   namespace :v1, defaults: { format: :json } do
     resources :verticals, except: %i[new edit]
     resources :categories, except: %i[new edit]
